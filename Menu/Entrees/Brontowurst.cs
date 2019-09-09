122e8706c704800713c08f88e390eea4e9512d91
tjr2020 <tjr2020@F1092-31-2119>
2019-09-09 17:02:02 -0500

    first_commit

---
+++ Menu/Entrees/Brontowurst.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+
+
+namespace DinoDiner.Menu.Entrees
+{
+    public class Brontowurst
+    {
+        private bool Bun = true;
+        private bool Peppers = true;
+        private bool Onions = true;
+
+        public double Price { get; set; }
+        public uint Calories { get; set; }
+
+        public List<string> Ingredients
+        {
+            get
+            {
+                List<string> ingredients = new List<string>() { "Whole Wheath Bun", "Brautwurst", "Peppers" , "Onions"};
+                return ingredients;
+            }
+        }
+
+        public Brontowurst()
+        {
+            this.Bun = 5.36;
+            this.Calories = 498;
+        }
+
+        public void HoldBun()
+        {
+            this.Bun = false;
+        }
+
+        public void HoldPeppers()
+        {
+            this.Peppers = false;
+        }
+        public void HoldOnions()
+        {
+            this.Onions = false;
+        }
+    }
+}

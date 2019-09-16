4b835f66c8ffdf983fb67346ac878a3e9518cfcd
tjr2020 <tjr2020@CIS-RDS2>
2019-09-15 20:19:27 -0500

    ?? maybe this work this time

---
+++ Menu/Entrees/SteakosaurusBurger.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+
+
+namespace DinoDiner.Menu.Entrees
+{
+    public class SteakosaurusBurger
+    {
+        public double Price { get; set; }
+        public uint Calories { get; set; }
+
+        public bool Bun = true;
+        public bool Pickle = true;
+        public bool Ketchup = true;
+        public bool Mustard = true;
+        public List<string> Ingredients
+        {
+            get
+            {
+                List<string> ingredients = new List<string>() { "Steakburger Pattie" };
+                if (Bun) ingredients.Add("Whole Wheat Bun");
+                if (Pickle) ingredients.Add("Pickle");
+                if (Ketchup) ingredients.Add("Ketchup");
+                if (Mustard) ingredients.Add("Mustard");
+                return ingredients;
+            }
+        }
+
+        public SteakosaurusBurger()
+        {
+            this.Price = 5.15;
+            this.Calories = 621;
+        }
+
+        public void HoldBun()
+        {
+            Bun = false;
+        }
+
+        public void HoldPickle()
+        {
+            Pickle = false;
+        }
+        public void HoldKetchup()
+        {
+            Ketchup = false;
+        }
+        public void HoldMustard()
+        {
+            Mustard = false;
+        }
+    }
+}
---
+++ Menu/Entrees/TRexKingBurger.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+
+
+namespace DinoDiner.Menu.Entrees
+{
+    public class TRexKingBurger
+    {
+        public double Price { get; set; }
+        public uint Calories { get; set; }
+
+        public bool Bun = true;
+        public bool Lettuce = true;
+        public bool Tomato = true;
+        public bool Onion = true;
+        public bool Pickle = true;
+        public bool Ketchup = true;
+        public bool Mustard = true;
+        public bool Mayo = true;
+        public List<string> Ingredients
+        {
+            get
+            {
+                List<string> ingredients = new List<string>() { "Steakburger Pattie" };
+                if (Bun) ingredients.Add("Whole Wheat Bun");
+                if (Lettuce) ingredients.Add("Lettuce");
+                if (Tomato) ingredients.Add("Tomato");
+                if (Mustard) ingredients.Add("Mustard");
+                if (Mustard) ingredients.Add("Mustard");
+                if (Mustard) ingredients.Add("Mustard");
+                if (Mustard) ingredients.Add("Mustard");
+
+                return ingredients;
+            }
+        }
+
+        public TRexKingBurger()
+        {
+            this.Price = 8.45;
+            this.Calories = 728;
+        }
+
+        public void HoldBun()
+        {
+            Bun = false;
+        }
+
+        public void HoldLettuce()
+        {
+            Lettuce = false;
+        }
+        public void HoldTomato()
+        {
+            Tomato = false;
+        }
+        public void HoldOnion()
+        {
+            Onion = false;
+        }
+        public void HoldPickle()
+        {
+            Pickle = false;
+        }
+        public void HoldKetchup()
+        {
+            Ketchup = false;
+        }
+        public void HoldMustard()
+        {
+            Mustard = false;
+        }
+        public void HoldMayo()
+        {
+            Mayo = false;
+        }
+    }
+}

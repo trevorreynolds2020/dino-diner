c0746e3268c27d83457449570b7b7e22ea7f6a7d
tjr2020 <tjr2020@F1092-33-2121>
2019-09-23 17:00:19 -0500

    drinks

---
+++ Menu/Drinks/Drink.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DinoDiner.Menu.Drinks
+{
+    public abstract class Drink
+    {
+        protected List<string> ingredients = new List<string>();
+
+        // define enum for size?
+       // public Size size { get; set; }
+        public double Price { get; set; }
+        public uint Calories { get; set; }
+        public List<string> Ingredients { get { return ingredients; } }
+        public bool Ice { get; set; }
+
+        public void HoldIce()
+        {
+            Ice = false;
+        }
+    }
+}
---
+++ Menu/Drinks/Water.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DinoDiner.Menu.Drinks
+{
+    public class Water: Drink
+    {
+        public Water()
+        {
+            Price = 0;
+            Calories = 0;
+            Ice = true;
+        }
+
+        public void AddLemon()
+        {
+
+        }
+    }
+}

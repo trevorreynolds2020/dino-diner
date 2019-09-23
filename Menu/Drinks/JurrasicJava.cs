3a7ad2019ff103c7734a768c677d00b1368f7934
tjr2020 <tjr2020@F1092-33-2121>
2019-09-23 17:07:32 -0500

    made all the drink classes, still need correct info and tests

---
+++ Menu/Drinks/JurrasicJava.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DinoDiner.Menu.Drinks
+{
+    public class JurrasicJava:Drink
+    {
+        public JurrasicJava()
+        {
+            Price = 0;
+            Calories = 0;
+            Ice = true;
+        }
+    }
+}
---
+++ Menu/Drinks/Sodasaurus.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DinoDiner.Menu.Drinks
+{
+    public class Sodasaurus : Drink
+    {
+        public enum SadasaurusFlavor
+        {
+            Cola,
+            Orange,
+            Rootbear,
+            Lime,
+            Cherry,
+            Vanilla,
+            Grape,
+            Cream
+        }
+        public Sodasaurus()
+        {
+            Price = 0;
+            Calories = 0;
+            Ice = true;
+        }
+    }
+}

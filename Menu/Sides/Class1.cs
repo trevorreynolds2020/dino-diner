00291f425d0298344570f64af3158a52c4a920c6
tjr2020 <tjr2020@CIS-RDS3>
2019-09-22 21:05:04 -0500

    hello

---
+++ Menu/Sides/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DinoDiner.Menu.Sides
+{
+    public class Frycertiops : Side
+    {
+        private Size size;
+
+        public override Size Size
+        {
+            set
+            {
+                size = value;
+                switch (size)
+                {
+                    case Size.Small:
+                        Price = 0.99;
+                        Calories = 222;
+                        break;
+                    case Size.Medium:
+                        Price = 1.45;
+                        Calories = 365;
+                        break;
+                    case Size.Large:
+                        Price = 1.95;
+                        Calories = 480;
+                        break;
+                }
+            }
+            get { return size; }
+        }
+       
+
+        public Frycertiops()
+        {
+            Price = 0.99;
+            Calories = 352;
+            Ingredients.Add("Potato");
+            Ingredients.Add("Salt");
+            Ingredients.Add("Vegetable oil");
+        }
+    }
+}

d69090d41f12526d4a4094ee3f0dff380c332059
tjr2020 <tjr2020@CIS-RDS1>
2019-10-13 21:54:22 -0500

    DONE

--- PointOfSale/DrinkSelection.xaml.cs
+++ PointOfSale/DrinkSelection.xaml.cs
@@ -24,15 +24,76 @@ namespace PointOfSale
         {
             InitializeComponent();
         }
-        void SelectFlavor(object sender, RoutedEventArgs args)
+
+        Button FlavorButton = new Button();
+        Button LemonButton = new Button();
+        Button SweetnerButton = new Button();
+        Button HoldIce = new Button();
+        Button RoomForCream = new Button();
+        Button AddIce = new Button();
+        Button Decaf = new Button();
+
+        
+
+        void SodasaurusClick(object sender, RoutedEventArgs args)
+        {
+            RemoveAllButtons();
+            FlavorButton.Content = "Flavor";
+            SelectDrink.Children.Add(FlavorButton);
+            FlavorButton.Click += new RoutedEventHandler(FlavorScreen);
+       
+        }
+        void FlavorScreen(object sender, RoutedEventArgs args)
         {
             NavigationService.Navigate(new FlavorSelection());
         }
-        void HoldIce(object sender, RoutedEventArgs args)
+        void JurassicJavaClick(object sender, RoutedEventArgs args)
         {
-            //NavigationService.Navigate(new FlavorSelection());
-            //this.SelectDrink.Add(new Button() { Name = "Hold Ice" });
+            RemoveAllButtons();
+            RoomForCream.Content = "Leave Room";
+            AddIce.Content = "Add Ice";
+            Decaf.Content = "Decaf";
+            SelectDrink.Children.Add(RoomForCream);
+            SelectDrink.Children.Add(AddIce);
+            SelectDrink.Children.Add(Decaf);
+            //.Click += new RoutedEventHandler();
+
         }
+        
+        void WaterClick(object sender, RoutedEventArgs args)
+        {
+            RemoveAllButtons();
+            AddIce.Content = "Add Ice";
+            SelectDrink.Children.Add(AddIce);
+            //.Click += new RoutedEventHandler();
+        }
+        void TyrannoteaClick(object sender, RoutedEventArgs args)
+        {
+            RemoveAllButtons();
+            SweetnerButton.Content = "Add Sweetner";
+            AddIce.Content = "Add Ice";
+            LemonButton.Content = "Add Lemon";
+            SelectDrink.Children.Add(LemonButton);
+            SelectDrink.Children.Add(AddIce);
+            SelectDrink.Children.Add(SweetnerButton);
+
+            //.Click += new RoutedEventHandler();
+        }
+        void AddSweetner(object sender, RoutedEventArgs args)
+        {
+            // adds sweetner
+        }
+        void RemoveAllButtons()
+        {
+            SelectDrink.Children.Remove(FlavorButton);
+            SelectDrink.Children.Remove(LemonButton);
+            SelectDrink.Children.Remove(SweetnerButton);
+            SelectDrink.Children.Remove(HoldIce);
+            SelectDrink.Children.Remove(RoomForCream);
+            SelectDrink.Children.Remove(AddIce);
+            SelectDrink.Children.Remove(Decaf);
+        }
+        
 
     }
 }

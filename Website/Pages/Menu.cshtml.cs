9d4cc20637f2b1e4bec7fa3cd4c6bd957e678080
tjr2020 <tjr2020@E1117W17>
2019-11-18 15:03:29 -0600

    trying to entree list on Menu object in webpage, crashes every time I call it

---
+++ Website/Pages/Menu.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using DinoDiner.Menu;
+namespace Website.Pages
+{
+    public class MenuModel : PageModel
+    {
+        protected Menu menu;
+        public Menu Menu { get { return menu; } }
+        public void OnGet()
+        {
+            menu = new Menu();
+        }
+    }
+}
